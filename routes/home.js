1cc4ff8a56961842eee253c523e8e32c98f3b6f9
sudhanshu <sudhanshuat6324@gmail.com>
2019-09-16 12:08:25 +0530

    fixed forgot signup etc

--- routes/home.js
+++ routes/home.js
@@ -5,11 +5,12 @@ const express = require('express'),
 router.get('/',async (req,res,next) =>{
     
     try{       
-        if(req.user){
-            res.render('index.ejs',{user  : req.user});
-        }else{
-            res.render('home.ejs',{user : null})
-        }
+        res.render('home1.ejs',{user : null})
+        // if(req.user){
+        //     res.render('index.ejs',{user  : req.user});
+        // }else{
+        //     res.render('home.ejs',{user : null})
+        // }
        // res.render('play.ejs',{user : null,videos : null})
         //res.send(");
         //res.send(req.user);
